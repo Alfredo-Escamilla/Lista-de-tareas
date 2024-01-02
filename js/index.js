29c6294fb30050a5145c9d10259e828fd43f9fe7
Alfredo Escamilla <148432892+Alfredo-Escamilla@users.noreply.github.com>
2024-01-02 18:52:12 +0100

    completar: Fase I

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ let objetoArray;
 let nuevaTarea;
 let conversionRegistro;
 let arrayTemporal = [];
-let nuevoRegistro;
+let tareaNueva;
 let textoTarea;
 let tareaId;
 let tareaParaEditar;
@@ -17,107 +17,13 @@ function cargaDeDatos() {
   leerValoresJson().then(() => {
     for (let i = 0; i < arrayTemporal.length; i++) {
       textoTarea = arrayTemporal[i].tarea;
-      crearElementoCard(arrayTemporal[i]);
+      creacionNuevaTarea(arrayTemporal[i]);
     }
   })
 
 }
 
-async function guardarDatosEnJson(tarea) {
-  const rutaJsonDeVuelta = "http://localhost:3000/tareas";
-  try {
-    const responseVuelta = await fetch(rutaJsonDeVuelta, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(tarea),
-    });
-  } catch (error) {
-  }
-}
-
-function capturarValoresIntroducidos() {
-  valorCajaTarea = document.getElementById("cajaTextoTarea").value;
-  valorCajaNotas = document.getElementById("cajaTextoNotas").value;
-}
-
-function leerValoresJson() {
-  return new Promise(async (resolve) => {
-    try {
-      const response = await fetch(rutaJson);
-      const responseData = await response.json();
-      arrayTemporal.length = 0;
-      arrayTemporal = [...responseData.tareas];
-      resolve()
-    } catch (error) {
-      let errorJson = `
-      <div class="alert alert-warning alert-dismissible fade show" role="alert" style="margin-left: 1.3em; margin-right: 1.3em; width: 91%;">
-        ${error.message}
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-      </div> `;
-      nuevaTarea.insertAdjacentHTML("beforeend", errorJson);
-    }
-  })
-}
-
-
-function editarTarea(tareaId) {
-  recuperarTarea(tareaId).then((tarea) => {
-    ventanaModal(tarea);
-  });
-
-}
-
-function recuperarTarea(tareaId) {
-  const apiUrl = `http://localhost:3000/tareas/${tareaId}`;
-  return new Promise(async (resolve) => {
-    try {
-      const response = await fetch(apiUrl);
-      const tarea = await response.json();
-      resolve(tarea)
-    } catch (error) {
-      let errorJson = `
-      <div class="alert alert-warning alert-dismissible fade show" role="alert" style="margin-left: 1.3em; margin-right: 1.3em; width: 91%;">
-        ${error.message}
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-      </div> `;
-    }
-  })
-
-}
-
-async function borrarTarea(tareaId) {
-  if (window.confirm('¿Estás seguro de que deseas eliminar esta tarea?')) {
-    const rutaJsonDeVuelta = `http://localhost:3000/tareas/${tareaId}`;
-    try {
-      const responseVuelta = await fetch(rutaJsonDeVuelta, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json',
-        }
-      });
-    } catch (error) {
-      console.error('Error en la solicitud de eliminación:', error);
-    }
-  } else {
-    console.log('La tarea no ha sido eliminada');
-  }
-}
-
-function guardarTareaEnArrayTemporal() {
-  indiceArray = arrayTemporal.length + 1;
-  nuevoRegistro = {
-    id: indiceArray,
-    tarea: valorCajaTarea,
-    notas: valorCajaNotas,
-    completada: completada,
-    eliminada: eliminada
-  };
-  arrayTemporal.push(nuevoRegistro);
-}
-
-function crearElementoCard(arrayTemporal) {
+function creacionNuevaTarea(arrayTemporal) {
   let nombreTarea = arrayTemporal.tarea.slice(0, 35).padEnd(35, " ");
   nuevaTarea = document.querySelector("#objetoTarea");
   let elementos = `
@@ -174,13 +80,91 @@ function ventanaModal(tarea) {
 }
 
 
-// botonGuardarTarea.addEventListener("click", function () {
 function botonGuardarTarea() {
   capturarValoresIntroducidos();
   guardarTareaEnArrayTemporal();
-  crearElementoCard(nuevoRegistro);
-  guardarDatosEnJson(nuevoRegistro);
+  creacionNuevaTarea(tareaNueva);
+  guardarDatosEnJson(tareaNueva);
 }
+
+function leerValoresJson() {
+  return new Promise(async (resolve) => {
+    try {
+      const response = await fetch(rutaJson);
+      const responseData = await response.json();
+      arrayTemporal.length = 0;
+      arrayTemporal = [...responseData.tareas];
+      resolve()
+    } catch (error) {
+      let errorJson = `
+      <div class="alert alert-warning alert-dismissible fade show" role="alert" style="margin-left: 1.3em; margin-right: 1.3em; width: 91%;">
+        ${error.message}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div> `;
+      nuevaTarea.insertAdjacentHTML("beforeend", errorJson);
+    }
+  })
+}
+
+async function guardarDatosEnJson(tarea) {
+  const rutaJsonDeVuelta = "http://localhost:3000/tareas";
+  try {
+    const responseVuelta = await fetch(rutaJsonDeVuelta, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(tarea),
+    });
+  } catch (error) {
+  }
+}
+
+function capturarValoresIntroducidos() {
+  valorCajaTarea = document.getElementById("cajaTextoTarea").value;
+  valorCajaNotas = document.getElementById("cajaTextoNotas").value;
+}
+
+
+
+function editarTarea(tareaId) {
+  recuperarTarea(tareaId).then((tarea) => {
+    ventanaModal(tarea);
+  });
+
+}
+
+function recuperarTarea(tareaId) {
+  const apiUrl = `http://localhost:3000/tareas/${tareaId}`;
+  return new Promise(async (resolve) => {
+    try {
+      const response = await fetch(apiUrl);
+      const tarea = await response.json();
+      resolve(tarea)
+    } catch (error) {
+      let errorJson = `
+      <div class="alert alert-warning alert-dismissible fade show" role="alert" style="margin-left: 1.3em; margin-right: 1.3em; width: 91%;">
+        ${error.message}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div> `;
+    }
+  })
+
+}
+
+
+function guardarTareaEnArrayTemporal() {
+  indiceArray = arrayTemporal.length + 1;
+  tareaNueva = {
+    id: indiceArray,
+    tarea: valorCajaTarea,
+    notas: valorCajaNotas,
+    completada: completada,
+    eliminada: eliminada
+  };
+  arrayTemporal.push(tareaNueva);
+}
+
 
 function botonActualizarTarea(tareaId) {
   capturarValoresIntroducidos();
@@ -189,8 +173,51 @@ function botonActualizarTarea(tareaId) {
   });
 }
 
+async function borrarTarea(tareaId) {
+  if (window.confirm('¿Estás seguro de que deseas eliminar esta tarea?')) {
+    const rutaJsonDeVuelta = `http://localhost:3000/tareas/${tareaId}`;
+    try {
+      const responseVuelta = await fetch(rutaJsonDeVuelta, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      });
+    } catch (error) {
+      console.error('Error en la solicitud de eliminación:', error);
+    }
+  } else {
+    console.log('La tarea no ha sido eliminada');
+  }
+}
+
+async function cambiarEstadoCompletado(tareaId) {
+  if (window.confirm('¿Estás seguro de marcar como completada la tarea?')) {
+    try {
+      const tarea = await recuperarTarea(tareaId);
+      console.log(tarea);
+      console.log(tarea.completada);
+      tarea.completada = tarea.completada ? false : true;
+      console.log(tarea.completada);
+      console.log(tarea);
+    } catch (error) {
+      console.error('Ocurrió un error al completar la tarea:', error);
+    }
+  } else {
+    alert('La tarea no ha sido completada');
+  }
+}
+
+
+async function completarTarea(tareaId) {
+  recuperarTarea(tareaId);
+  cambiarEstadoCompletado(tareaId);
+  actualizarJson(tareaId);
+}
+
+
 async function actualizarJson(tareaId) {
-  const apiUrl = `http://127.0.0.1:3000/tareas/${tareaId}`; //http://localhost:3000/tareas
+  const apiUrl = `http://127.0.0.1:3000/tareas/${tareaId}`; 
   const tarea = {
     id: tareaId,
     tarea: valorCajaTarea,

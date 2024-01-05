4383929ea6d1bd1960d29c429289578903aef8cf
Alfredo Escamilla <148432892+Alfredo-Escamilla@users.noreply.github.com>
2024-01-05 12:30:34 +0100

    login working

---
+++ js/login.js
@@ -0,0 +1,52 @@
+let arrayTemporal = [];
+
+function login(usuario, password) {
+    usuario = document.getElementById('usuario').value;
+    password = document.getElementById('password').value;
+    cargaDeDatos();
+}
+
+function cargaDeDatos() {
+    console.log('carga de datos función');
+    leerJson().then(() => {
+      for (let i = 0; i < arrayTemporal.length; i++) {
+        let idUser = arrayTemporal[i].idUser;
+        let user = arrayTemporal[i].user;
+        let pass = arrayTemporal[i].pass;
+        console.log('ID User: ' + idUser);
+        console.log('Usuario: ' + user);
+        console.log('Password: ' + pass);
+        alert('STOP');
+      }
+    })
+   
+  }
+
+  async function leerJson() {
+    try {
+        const rutaJson = "/json/datos.json";
+        console.log('Iniciando lectura de datos');
+        const response = await fetch(rutaJson);
+        const responseData = await response.json();
+        arrayTemporal.length = 0;
+        arrayTemporal = [...responseData.usuarios];
+        console.log('Array temporal:', arrayTemporal);
+    } catch (error) {
+        console.error('Error en lectura de datos:', error);
+    }
+}
+
+//   function leerJson() {
+//     return new Promise(async (resolve) => {
+//       try {
+//         const response = await fetch(rutaJson);
+//         const responseData = await response.json();
+//         arrayTemporal.length = 0;
+//         arrayTemporal = [...responseData.usuarios];
+//         console.log('Array temporal: ' + arrayTemporal);
+//         resolve()
+//       } catch (error) {
+//         console.error('Error en lectura de datos');
+//       }
+//     })
+//   }
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('/json/datos.json');
+const router = jsonServer.router('json/datos.json');
 const middlewares = jsonServer.defaults();
 const cors = require('cors');
 

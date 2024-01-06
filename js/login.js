3a6d772081880e8aee71a09c568990d728b6165d
Alfredo Escamilla <148432892+Alfredo-Escamilla@users.noreply.github.com>
2024-01-06 09:31:37 +0100

    Lectura de login OK

--- js/login.js
+++ js/login.js
@@ -1,35 +1,16 @@
+const rutaJson = "json/datos.json";
 let arrayTemporal = [];
 
 function login(usuario, password) {
     usuario = document.getElementById('usuario').value;
     password = document.getElementById('password').value;
-    cargaDeDatos(usuario, password);
+    console.log('Usuario introducido: ' + usuario);
+    console.log('Password introducida: ' + password);
+    cargaDeDatos();
 }
 
-// function cargaDeDatos(usuario, password) {
-//     console.log('carga de datos función');
-//     console.log('Usuario introducido: ' + usuario);
-//     console.log('Password introducida: ' + password)
-//     leerJson().then(() => {
-//       for (let i = 0; i < arrayTemporal.length; i++) {
-//         let idUser = arrayTemporal[i].idUser;
-//         let user = arrayTemporal[i].user;
-//         let pass = arrayTemporal[i].pass;
-//         console.log('ID User: ' + idUser);
-//         console.log('Usuario: ' + user);
-//         console.log('Password: ' + pass);
-//         console.log('Usuario introducido: ' + usuario);
-//         console.log('Password introducida: ' + password)
-//         alert('STOP');
-//       }
-//     })
-   
-//   }
-
-  function cargaDeDatos(usuario, password) {
+function cargaDeDatos() {
     console.log('carga de datos función');
-    console.log('Usuario introducido: ' + usuario);
-    console.log('Password introducida: ' + password)
     leerJson().then(() => {
       for (let i = 0; i < arrayTemporal.length; i++) {
         let idUser = arrayTemporal[i].idUser;
@@ -38,26 +19,24 @@ function login(usuario, password) {
         console.log('ID User: ' + idUser);
         console.log('Usuario: ' + user);
         console.log('Password: ' + pass);
-        console.log('Usuario introducido: ' + usuario);
-        console.log('Password introducida: ' + password)
         alert('STOP');
       }
     })
    
   }
 
-
   async function leerJson() {
     try {
-        const rutaJson = "json/datos.json";
         console.log('Iniciando lectura de datos');
         const response = await fetch(rutaJson);
         const responseData = await response.json();
         arrayTemporal.length = 0;
         arrayTemporal = [...responseData.usuarios];
-        console.log('Array temporal:', arrayTemporal);
+        console.log('Array temporal:', arrayTemporal[0]);
     } catch (error) {
         console.error('Error en lectura de datos:', error);
+        alert('STOP CATCH');
+
     }
 }
 
